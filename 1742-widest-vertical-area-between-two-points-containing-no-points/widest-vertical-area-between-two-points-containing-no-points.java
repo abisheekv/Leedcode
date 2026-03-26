2016acb1a4832c2075472ef44b93a15c7cf16a61
abisheekv <abisheekv2006@gmail.com>
2026-03-26 21:12:50 +0530

    Time: 16 ms (97.88%) | Memory: 89.5 MB (60.88%) - LeetSync

---
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        int n = points.length;
+        
+        int[] x = new int[n];
+        
+        // Extract x-coordinates
+        for (int i = 0; i < n; i++) {
+            x[i] = points[i][0];
+        }
+        
+        // Sort x values
+        Arrays.sort(x);
+        
+        int maxGap = 0;
+        
+        // Find maximum gap
+        for (int i = 1; i < n; i++) {
+            maxGap = Math.max(maxGap, x[i] - x[i - 1]);
+        }
+        
+        return maxGap;
+    }
+}
