2db1eaf31977fd5f88be07f10ee7501a342a3d4b
abisheekv <abisheekv2006@gmail.com>
2026-08-05 21:18:07 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (11.32%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -6,22 +6,32 @@ class Solution {
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
+            // Target found
             if (nums[mid] == target) {
                 return mid;
             }
 
+            // Left half is sorted
             if (nums[left] <= nums[mid]) {
+
+                // Target lies in left half
                 if (target >= nums[left] && target < nums[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-            } else {
+
+            }
+            // Right half is sorted
+            else {
+
+                // Target lies in right half
                 if (target > nums[mid] && target <= nums[right]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
                 }
+
             }
         }
 
