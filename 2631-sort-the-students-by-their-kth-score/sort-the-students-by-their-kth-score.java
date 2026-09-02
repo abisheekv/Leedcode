f2e14728f6679cd0b957ceb06358e48c3847c460
abisheekv <abisheekv2006@gmail.com>
2026-09-02 09:32:54 +0530

    Time: 3 ms (83.96%) | Memory: 56.1 MB (46.59%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
-        Arrays.sort(score, (a, b) -> b[k] - a[k]);
+        // Sort rows in descending order based on the value at index k
+        Arrays.sort(score, (a, b) -> Integer.compare(b[k], a[k]));
         return score;
     }
 }
