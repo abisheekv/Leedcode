8193c2dd777c7378f699f0425b188413e212a336
abisheekv <abisheekv2006@gmail.com>
2026-09-02 09:28:47 +0530

    Time: 1 ms (99.88%) | Memory: 45.5 MB (67.64%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,22 +1,23 @@
-
-
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] sorted = nums.clone();
-        Arrays.sort(sorted);
-
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for(int i = 0; i < sorted.length; i++) {
-            map.putIfAbsent(sorted[i], i);
+        int[] count = new int[102];
+        
+        // 1. Count occurrences of each number
+        for (int num : nums) {
+            count[num + 1]++;
         }
-
+        
+        // 2. Compute prefix sum
+        for (int i = 1; i < count.length; i++) {
+            count[i] += count[i - 1];
+        }
+        
+        // 3. Construct result array
         int[] result = new int[nums.length];
-
-        for(int i = 0; i < nums.length; i++) {
-            result[i] = map.get(nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = count[nums[i]];
         }
-
+        
         return result;
     }
 }
