1086a3280132a1f2e865cd20aad19264f5ffc611
abisheekv <abisheekv2006@gmail.com>
2026-09-17 09:45:48 +0530

    Time: 1 ms (98.75%) | Memory: 46.8 MB (57.11%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        for (int step = 0; step < k; step++) {
+        
+        for (int op = 0; op < k; op++) {
+            
+            // Step 1: find index of minimum element (first occurrence)
             int minIndex = 0;
             
-            // Find the index of the first minimum value
             for (int i = 1; i < nums.length; i++) {
                 if (nums[i] < nums[minIndex]) {
                     minIndex = i;
                 }
             }
             
-            // Multiply the minimum element
-            nums[minIndex] *= multiplier;
+            // Step 2: multiply it
+            nums[minIndex] = nums[minIndex] * multiplier;
         }
         
         return nums;
